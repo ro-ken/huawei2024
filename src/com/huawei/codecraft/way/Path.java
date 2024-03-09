620af91cbaad6d6eddc83e6f5f83b245461540b4
ro_xx_win11 <rq8866@126.com>
2024-03-09 21:44:35 +0800

    path增加新接口

--- src/com/huawei/codecraft/way/Path.java
+++ src/com/huawei/codecraft/way/Path.java
@@ -4,6 +4,7 @@ package com.huawei.codecraft.way;
 import com.huawei.codecraft.util.Point;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 // 静态接口类，里面的方法上层调用，下层实现
 public interface Path {
@@ -17,4 +18,6 @@ public interface Path {
     // 获取机器人去泊口最佳的路径。
     ArrayList<Point> getToBerthPath(Point robotPos,Point BerthPoint);
 
+    // 拥有新障碍下的路径
+    ArrayList<Point> getPathWithBarrier(Point p1,Point p2, HashSet<Point> barriers);
 }
