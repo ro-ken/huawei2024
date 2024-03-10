9651153a85d186846bd2c56f3f4b75345cc4c1c0
zhiqizhou <2637184812@qq.com>
2024-03-10 16:54:38 +0800

    提交遗漏文件

--- src/com/huawei/codecraft/way/Pos.java
+++ src/com/huawei/codecraft/way/Pos.java
@@ -6,13 +6,19 @@ public class Pos {
     public Point pos;
     public Pos father;  // 注意，father 应该是 Pos 类型
 
-    public Pos(Point p) {
-        this.pos = p;
-        this.father = null;
-    }
+    public int g;       // 从起点到当前点的成本
+    public int h;       // 从当前点到终点的启发式估计成本
 
-    public Pos(Point pos, Pos father) {
+    // 构造方法也需要相应地调整，以接受g和h作为参数
+    public Pos(Point pos, Pos father, int g, int h) {
         this.pos = pos;
         this.father = father;
+        this.g = g;
+        this.h = h;
+    }
+
+    // 一个方法来计算f值
+    public int f() {
+        return g + h;
     }
 }
